255f0336eea1c070df8193d4d65bafea0919b3ba
RomanLevkovych <rlevkovych098@gmail.com>
2019-11-24 23:38:11 +0200

    Placeholder text
    Implemented placeholder text functionality for log in navbar

--- View/Widgets/LogInNavbar.xaml.cs
+++ View/Widgets/LogInNavbar.xaml.cs
@@ -20,9 +20,42 @@ namespace View.Widgets
     /// </summary>
     public partial class LogInNavbar : UserControl
     {
+        private Dictionary<Control, string> _placeholderText;
         public LogInNavbar()
         {
             InitializeComponent();
+            _placeholderText = new Dictionary<Control, string>
+            {
+                { usernameTextBox, "Username" },
+                { passwordTextBox, "Password" }
+            };
+
+            passwordTextBox.GotFocus += RemoveText;
+            usernameTextBox.GotFocus += RemoveText;
+
+            passwordTextBox.LostFocus += AddText;
+            usernameTextBox.LostFocus += AddText;
+
+            AddText(passwordTextBox, null);
+            AddText(usernameTextBox, null);
+        }
+
+        private void AddText(object sender, EventArgs e)
+        {
+            TextBox senderBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(senderBox.Text))
+            {
+                senderBox.Text = _placeholderText[senderBox];
+            }
+        }
+
+        private void RemoveText(object sender, EventArgs e)
+        {
+            TextBox senderBox = sender as TextBox;
+            if (senderBox.Text.Equals(_placeholderText[senderBox]))
+            {
+                senderBox.Text = "";
+            }
         }
     }
 }
