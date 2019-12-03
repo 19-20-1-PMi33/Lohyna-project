02cafc91c5d199961b989583534d52f1335a2e62
Nikita <n0962783666@gmail.com>
2019-12-04 00:22:23 +0200

    Auto stash before merge of "development" and "feature/log-in-page-update"

--- ViewModel/RegisterPageVM.cs
+++ ViewModel/RegisterPageVM.cs
@@ -1,10 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using DataServices.Services;
 
 namespace ViewModel
 {
-    class RegisterPageVM
+    public class RegisterPageVM
     {
+        private IPersonService service;
+        public RegisterPageVM(IPersonService service)
+        {
+            this.service = service;
+        }
+        public string copyImage(string fileName,string nom_zal)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string destFolder = Path.Combine("..","..","..", "View", "Widgets", "Images", "Profile_Images");
+            string destFile = Path.Combine(destFolder, nom_zal + Path.GetExtension(fileName));
+            File.Copy(fileName, destFile);
+            return destFile;
+        }
+        public bool validateValues(string name, string surname, string zal, string username, string password, string passwordRepeat)
+        {
+            if (!String.IsNullOrWhiteSpace(name))
+            {
+                return false;
+            }
+            if (!String.IsNullOrWhiteSpace(surname))
+            {
+                return false;
+            }
+            if (!String.IsNullOrWhiteSpace(zal))
+            {
+                return false;
+            }
+            if (!String.IsNullOrWhiteSpace(username))
+            {
+                return false;
+            }
+            if (!String.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+            if (password != passwordRepeat)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool registerUser(string name, string surname, string zal, string username, string password, string photo)
+        {
+            
+            return true;
+        }
     }
 }
