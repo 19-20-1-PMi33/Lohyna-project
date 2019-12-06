68c0a5b8bef4d3c5c0a01f09e9a6516ec7efdc08
OlegAndrus12 <andrusoleg123@gmail.com>
2019-12-06 23:53:57 +0200

    Path to db changed

--- DataServices/SqliteDbContext.cs
+++ DataServices/SqliteDbContext.cs
@@ -16,7 +16,7 @@ namespace DataServices
         {
             var connectionStringBuilder = new SqliteConnectionStringBuilder();
             // TODO: create adequate location for DB.
-            connectionStringBuilder.DataSource = @"C:\Users\andru\Documents\GitHub\Lohyna-project\DataServices\university-db.db";
+            connectionStringBuilder.DataSource = @"..\..\..\..\DataServices\univerity-db.db";
             _connectionString = connectionStringBuilder.ConnectionString;
         }
 
