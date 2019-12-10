cb85aaaf2b988160541d38fbf82ff482a9a9b7e8
RomanLevkovych <rlevkovych098@gmail.com>
2019-12-10 19:13:42 +0200

    Removed DTO folder (unused) and doc for main serv

--- DataServices/IDataService.cs
+++ DataServices/IDataService.cs
@@ -3,6 +3,9 @@ using DataServices.Services;
 
 namespace DataServices
 {
+    /// <summary>
+    /// Global data service.
+    /// </summary>
     public interface IDataService : IDisposable, IFaqService, IMarkService,
         INoteService, IPersonService, ITimeTableService, ISubjectService, IGroupService
     {
--- DataServices/IDataSource.cs
+++ DataServices/IDataSource.cs
@@ -7,24 +7,90 @@ using Model;
 
 namespace DataServices
 {
+    /// <summary>
+    /// Data source for application.
+    /// </summary>
     public interface IDataSource : IDisposable
     {
+        /// <summary>
+        /// Cabinets table in data source.
+        /// </summary>
         DbSet<Cabinet> Cabinet { get; }
+
+        /// <summary>
+        /// FAQ table in data source.
+        /// </summary>
         DbSet<FAQ> FAQ { get; }
+
+        /// <summary>
+        /// Groups table in data source.
+        /// </summary>
         DbSet<Group> Group { get; }
+
+        /// <summary>
+        /// Lecturers table in data source.
+        /// </summary>
         DbSet<Lecturer> Lecturer { get; }
+
+        /// <summary>
+        /// News table in data source.
+        /// </summary>
         DbSet<News> News { get; }
+
+        /// <summary>
+        /// Notes table in data source.
+        /// </summary>
         DbSet<Note> Note { get; }
+
+        /// <summary>
+        /// People table in data source.
+        /// </summary>
         DbSet<Person> Person { get; }
+
+        /// <summary>
+        /// Ratings table in data source.
+        /// </summary>
         DbSet<Rating> Rating { get; }
+
+        /// <summary>
+        /// Students table in data source.
+        /// </summary>
         DbSet<Student> Student { get; }
+
+        /// <summary>
+        /// Time table in data source.
+        /// </summary>
         DbSet<Time> Time { get; }
+
+        /// <summary>
+        /// Timetable table in data source.
+        /// </summary>
         DbSet<Timetable> Timetable { get; }
+
+        /// <summary>
+        /// Subjects table in data source.
+        /// </summary>
         DbSet<Subject> Subject { get; }
 
+        /// <summary>
+        /// Load entry from data source.
+        /// </summary>
+        /// <typeparam name="TEntity">Model in DB.</typeparam>
+        /// <param name="entity">Entity to load from data source.</param>
+        /// <returns>Object from DB.</returns>
         EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
 
+        /// <summary>
+        /// Save all the data in DB synchronious.
+        /// </summary>
+        /// <returns></returns>
         int SaveChanges();
+
+        /// <summary>
+        /// Save all the data in DB asynchronous.
+        /// </summary>
+        /// <param name="cancellationToken">Cancel tocken.</param>
+        /// <returns>Status of operation.</returns>
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
