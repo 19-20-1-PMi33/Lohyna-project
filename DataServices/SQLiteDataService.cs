2eb34421bbfad55d7d0e983554ac9c6cf908fc30
Roman Levkovych <rlevkovych098@gmail.com>
2020-02-24 23:54:58 +0200

    Created new ASP.Net app instead of WPF one.

--- DataServices/SQLiteDataService.cs
+++ DataServices/SQLiteDataService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DataServices
+﻿namespace DataServices
 {
     /// <summary>
     /// Service for obtaining data from SQLite DB.
