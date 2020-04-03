f80b19c064fc2a049c90925f74a62f3be6c317ed
OlegAndrus12 <andrusoleg123@gmail.com>
2020-04-03 16:18:01 +0300

    DTO added

---
+++ Core/DTO/Timetable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Core.DTO
+{
+    public class Timetable
+    {
+        public int Id { get; set; }
+		public string Day { get; set; } 
+		public string Period { get; set; }
+
+		public int TimeID { get; set; }
+
+		public virtual Subject Subject { get; set; }
+		public string SubjectID { get; set; }
+
+		public virtual Group Group { get; set; }
+		public string GroupID { get; set; }
+
+		public virtual Lecturer Lecturer { get; set; }
+		public int LecturerID { get; set; }
+    }
+}
