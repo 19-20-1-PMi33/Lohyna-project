481f7a48a6da637e24404be44b6dab2dbef69a1c
OlegAndrus12 <andrusoleg123@gmail.com>
2020-04-03 17:33:05 +0300

    TimeTableController is created

---
+++ WebApplication/Controllers/TimetableController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Services.TimeTableService;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class TimetableController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ITimeTableService _timeTable;
+        private readonly ILogger<TimetableController> _logger;
+
+        public TimetableController(IMapper mapper, ITimeTableService timeTable, ILogger<TimetableController> logger)
+        {
+            _mapper = mapper;
+            _timeTable = timeTable;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
