7dc44a6830aa9f1cd136afb4165e545870ce2d9e
OlegAndrus12 <andrusoleg123@gmail.com>
2020-04-04 23:33:29 +0300

    Controller is ready

--- Services/TimeTableService/TimeTableService.cs
+++ Services/TimeTableService/TimeTableService.cs
@@ -13,7 +13,7 @@ namespace Services.TimeTableService
             {
                 new Timetable { Id = 1, Day = "Monday", GroupID = "PMI-33", TimeID = 1, SubjectID = "PO", LecturerID = 1, Period = " "},
                 new Timetable { Id = 2, Day = "Tuesday", GroupID = "PMI-33", TimeID = 2, SubjectID = "Numerical Methods", LecturerID = 1, Period = " "},
-                new Timetable { Id = 3, Day = "Monday", GroupID = "PMI-33", TimeID = 1, SubjectID = "   Cryptology", LecturerID = 1, Period = " "},
+                new Timetable { Id = 3, Day = "Wednesday", GroupID = "PMI-33", TimeID = 1, SubjectID = "Cryptology", LecturerID = 1, Period = " "},
                 new Timetable { Id = 4, Day = "Friday", GroupID = "PMI-33", TimeID = 4, SubjectID = "PO", LecturerID = 1, Period = " "}
             };   
             return listTimetable;
--- WebApplication/Controllers/TimetableController.cs
+++ WebApplication/Controllers/TimetableController.cs
@@ -6,6 +6,16 @@ using System.Collections.Generic;
 using Services.TimeTableService;
 using WebApplication.Models;
 using System.Linq;
+using System;
+
+public enum WeekDays
+{
+    Monday = 0,
+    Tuesday = 1,
+    Wednesday = 2,
+    Thursday = 3,
+    Friday = 4
+}
 
 namespace WebApplication.Controllers
 {
@@ -24,7 +34,28 @@ namespace WebApplication.Controllers
 
         public IActionResult Index()
         {
-            Dictionary<int, List<Core.DTO.Timetable>> keyValues = new Dictionary<int, List<Core.DTO.Timetable>>();
+            Dictionary<int, List<Core.DTO.Timetable>> keyValues;
+            List<Core.DTO.Timetable> ls = _timeTable.LoadTimetableAsync().Result;
+            keyValues = ls.GroupBy(x => x.TimeID)
+                .ToDictionary(x => x.Key, x => x.OrderBy(x => (int)((WeekDays)Enum.Parse(typeof(WeekDays), x.Day))).ToList())
+                ;
+            keyValues = keyValues.OrderBy(k => k.Key).ToDictionary(z => z.Key, y => y.Value);
+
+            foreach(var i in keyValues.Values)
+            {
+
+                for(int j = 0; j<i.Count; ++j)
+                {
+                    if ((int)((WeekDays)Enum.Parse(typeof(WeekDays), i[j].Day)) != j)
+                    {
+                        i.Insert(j, null);
+                    }
+                }
+                while (i.Count != 5)
+                {
+                    i.Insert(i.Count, null);
+                }
+            }
             
             return View("Timetable", keyValues);
         }
