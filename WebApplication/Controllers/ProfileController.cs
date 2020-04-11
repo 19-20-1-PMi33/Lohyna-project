252c1641c484946412d055e8d11558921d69d5aa
Nikita <n0962783666@gmail.com>
2020-04-11 15:03:37 +0300

    unused mapper removed, css in separate files

--- WebApplication/Controllers/ProfileController.cs
+++ WebApplication/Controllers/ProfileController.cs
@@ -10,15 +10,12 @@ namespace WebApplication.Controllers
     public class ProfileController : Controller
     {
         private readonly IMapper _mapper;
-
-        private readonly IMapper _personMapper;
         private readonly IProfileService _profile;
         private readonly ILogger<ProfileController> _logger;
 
         public ProfileController(IMapper mapper, IProfileService profile, ILogger<ProfileController> logger)
         {
-            _mapper = mapper;
-            _personMapper = new Mapper(new MapperConfiguration(cfg =>
+            _mapper = new Mapper(new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Core.DTO.Student, Models.ProfileViewModel>();
                 cfg.CreateMap<Core.DTO.Lecturer, Models.ProfileViewModel>();
@@ -33,10 +30,10 @@ namespace WebApplication.Controllers
             Core.DTO.Person userData = _profile.LoadPersonAsync(username).Result;
             ProfileViewModel model;
             if(userData.personType == Core.DTO.PersonType.student){
-                model = _personMapper.Map<ProfileViewModel>(userData as Core.DTO.Student);
+                model = _mapper.Map<ProfileViewModel>(userData as Core.DTO.Student);
             }
             else{
-                model = _personMapper.Map<ProfileViewModel>(userData as Core.DTO.Lecturer);
+                model = _mapper.Map<ProfileViewModel>(userData as Core.DTO.Lecturer);
             }
             return View("Profile", model);
         }
