f1e1d3d145b990209fcded2e28cc82809f3027ed
Roman Levkovych <rlevkovych098@gmail.com>
2020-04-21 18:47:23 +0300

    Fixed time display issue.

--- Services/NewsFeedService/NewsFeedService.cs
+++ Services/NewsFeedService/NewsFeedService.cs
@@ -10,12 +10,15 @@ namespace Services.NewsFeedService
     {
         public async Task<List<News>> LoadNewsAsync()
         {
-            DateTime time = DateTime.Now;
             string photo = "/DbResources/News/bitok.jpeg";
             string title = "📢Лекція «Блокчейн: як працює біткоїн»";
             string text = "Спікер заходу: Роман Левкович, студент 3 курсу факультету прикладної математики та інформатики🔝\n🗓Теми лекції:\n👉 що таке блокчейн❓\n👉 як створюють біткоїн та як це працює❓\n⏰ 6 березня, з 16:30 – 18:00\n📍 ауд.270\n💫Реєстрація обов'язкова!👇";
-            News newsItem = new News {Photo = photo, Text = text, Title = title, TimePosted = time, Id = 0};
-            return new List<News> {newsItem, newsItem, newsItem};
+            return new List<News>
+            {
+                new News {Photo = photo, Text = text, Title = title, TimePosted = DateTime.Now.AddSeconds(-5), Id = 0},
+                new News {Photo = photo, Text = text, Title = title, TimePosted = new DateTime(2020, 1, 10), Id = 0},
+                new News {Photo = photo, Text = text, Title = title, TimePosted = new DateTime(2018, 12, 25), Id = 0}
+            };
         }
     }
 }
