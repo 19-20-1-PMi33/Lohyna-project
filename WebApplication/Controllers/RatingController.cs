8a7828b13d89b8787b2855d722c72e5dbff010ac
OlegAndrus12 <andrusoleg123@gmail.com>
2020-04-21 13:54:50 +0300

    Basic Controller structure is added

---
+++ WebApplication/Controllers/RatingController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Services.RatingService;
+using WebApplication.Models;
+using System.Linq;
+using System;
+
+namespace WebApplication.Controllers
+{
+    public class RatingController: Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IRatingService _rating;
+        private readonly ILogger<RatingController> _logger;
+
+        public RatingController(IMapper mapper, IRatingService rating, ILogger<RatingController> logger)
+        {
+            _mapper = mapper;
+            _rating = rating;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return (View("Rating"));
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
