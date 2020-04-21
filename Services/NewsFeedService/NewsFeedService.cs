b1d1da78a668f6f94fd7fba5ca5203eb328d4bc6
Roman Levkovych <rlevkovych098@gmail.com>
2020-04-21 14:46:04 +0300

    Fixed images issue.

--- Services/NewsFeedService/NewsFeedService.cs
+++ Services/NewsFeedService/NewsFeedService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Core.DTO;
 
@@ -10,7 +11,7 @@ namespace Services.NewsFeedService
         public async Task<List<News>> LoadNewsAsync()
         {
             DateTime time = DateTime.Now;
-            string photo = "news1.jpg";
+            string photo = "/DbResources/News/bitok.jpeg";
             string title = "📢Лекція «Блокчейн: як працює біткоїн»";
             string text = "Спікер заходу: Роман Левкович, студент 3 курсу факультету прикладної математики та інформатики🔝\n🗓Теми лекції:\n👉 що таке блокчейн❓\n👉 як створюють біткоїн та як це працює❓\n⏰ 6 березня, з 16:30 – 18:00\n📍 ауд.270\n💫Реєстрація обов'язкова!👇";
             News newsItem = new News {Photo = photo, Text = text, Title = title, TimePosted = time, Id = 0};
--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-﻿using System.Diagnostics;
-using AutoMapper;
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 using Services.NewsFeedService;
 using WebApplication.Models;
 
@@ -9,20 +13,36 @@ namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IMapper _mapper;
+        private IHostEnvironment _host;
         private readonly INewsFeedService _newsFeed;
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IMapper mapper, INewsFeedService newsFeed, ILogger<HomeController> logger)
+        public HomeController(INewsFeedService newsFeed, IHostEnvironment host)
         {
-            _mapper = mapper;
+            _host = host;
             _newsFeed = newsFeed;
-            _logger = logger;
         }
 
-        public IActionResult Index()
+        private string EncodeImage(string path)
         {
-            return View();
+            using (Image image = Image.FromFile(path))
+            {
+                using (MemoryStream m = new MemoryStream())
+                {
+                    
+                    image.Save(m, image.RawFormat);
+                    byte[] imageBytes = m.ToArray();
+
+                    // Convert byte[] to Base64 String
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            }
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var news = _newsFeed.LoadNewsAsync().Result.Select(x => (x, EncodeImage(_host.ContentRootPath + x.Photo)));
+            return View(news);
         }
 
         public IActionResult Privacy()
