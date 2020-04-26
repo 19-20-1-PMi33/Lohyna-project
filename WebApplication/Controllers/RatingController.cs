467df11f76e9cfeda1ee477f568869bf6dcbecfb
OlegAndrus12 <andrusoleg123@gmail.com>
2020-04-26 19:34:05 +0300

    Last remake

--- WebApplication/Controllers/RatingController.cs
+++ WebApplication/Controllers/RatingController.cs
@@ -12,20 +12,16 @@ namespace WebApplication.Controllers
 {
     public class RatingController: Controller
     {
-        private readonly IMapper _mapper;
         private readonly IRatingService _rating;
-        private readonly ILogger<RatingController> _logger;
 
-        public RatingController(IMapper mapper, IRatingService rating, ILogger<RatingController> logger)
+        public RatingController(IRatingService rating)
         {
-            _mapper = mapper;
             _rating = rating;
-            _logger = logger;
         }
         public IActionResult Index()
         {
             List<Core.DTO.Rating> ratingsList = _rating.LoadRatingAsync().Result;
-            return (View("Rating", ratingsList));
+            return View("Rating", ratingsList);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
