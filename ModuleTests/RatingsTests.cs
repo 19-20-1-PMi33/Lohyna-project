6dcde5b0bb460ce7c775ba52ac6a8f28f42e8363
OlegAndrus12 <andrusoleg123@gmail.com>
2020-05-17 16:19:36 +0300

    Update RatingsTests.cs

--- ModuleTests/RatingsTests.cs
+++ ModuleTests/RatingsTests.cs
@@ -1,9 +1,19 @@
 using Xunit;
-
+using System.Collections.Generic;
+using Moq;
 namespace ModuleTests
 {
     public class RatingsTests
     {
+        [[Fact]
+        public void RatingsUploaded()
+        {
+            Mock<
+        //Given
         
+        //When
+        
+        //Then
+        }]
     }
 }
