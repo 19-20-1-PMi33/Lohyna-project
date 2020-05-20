2c26b732d5e39d70dc7c01bb9c2e3dd5e69c887f
Petro Tarnavskyi <petro.tarnavskyy@lnu.edu.ua>
2020-05-20 16:41:09 +0300

    Add page to change information about students

--- WebApplication/Controllers/AdminController.cs
+++ WebApplication/Controllers/AdminController.cs
@@ -5,25 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication.Models;
+using Model;
 
 namespace WebApplication.Controllers
 {
 	public class AdminController : Controller
 	{
-		public ActionResult Index()
+		public void getInformation()
 		{
 			ViewBag.Photo = "/images/user.png";
 			ViewBag.Name = "Oleh";
+
 			ViewBag.Role = "admin";
-			ViewBag.NumberOfStudents = 120;
 			ViewBag.NumberOfSubjects = 120;
 			ViewBag.NumberOfNews = 120;
 			ViewBag.NumberOfLectors = 120;
+
+			ViewBag.Students = new List<Student>();
+			ViewBag.NumberOfStudents = ViewBag.Students.Count;
+		}
+		public ActionResult Index()
+		{
+			getInformation();
 			return View();
 		}
 		public ActionResult AdminStudent()
 		{
-			return View();
+			getInformation();
+			return View("AdminStudent", ViewBag.Students);
 		}
 	}
 }
