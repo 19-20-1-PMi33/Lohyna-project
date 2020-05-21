5a6d4aba6e2f64f6e269799a75d1b43331e948dc
OlegAndrus12 <andrusoleg123@gmail.com>
2020-05-21 11:46:33 +0300

    Upd

--- Services/NotesService/INotesService.cs
+++ Services/NotesService/INotesService.cs
@@ -7,5 +7,9 @@ namespace Services.NotesService
     public interface INotesService
     {
         Task<IList<Model.Note>> LoadNotesAsync(string username);
+        Task CreateNoteAsync(Model.Note note);
+        Task<IList<Model.Subject>> LoadSubjectsAsync();
+        void DeleteNoteAsync(params Model.Note[] note);
+        void UpdateNoteAsync(Model.Note note);
     }
 }
